b7efeea12d2133cc02fef6814f6012c43847b508
Shift <shift@laravelshift.com>
2024-05-10 09:55:29 +0000

    Apply code style

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -23,6 +23,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('admin', fn($user) => $user->role === UserRole::Admin);
+        Gate::define('admin', fn ($user) => $user->role === UserRole::Admin);
     }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -37,5 +37,4 @@ class UserFactory extends Factory
             ];
         });
     }
-
 }
